b55588489c6c5e00ac95673a5c944417a19e3c89
Hazel195 <62048055+Hazel195@users.noreply.github.com>
2022-01-06 11:19:34 +0900

    initial push

---
+++ FoldYourDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoldYourDay
+//
+//  Created by Hazel Kim on 6/1/22.
+//  Copyright © 2022 Hazel Ki,. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
